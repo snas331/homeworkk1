890c2235bd983af64e90a916deb42165352f1c97
yakov23 <yakovoberderfer13@gmail.com>
2023-12-11 12:26:24 +0300

    file ignored

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 9000;
+        int bonus = 20;
+        System.out.println(9000 / 20);
+        int mil = 450;
+        System.out.println(mil);
+
+    }
+}
